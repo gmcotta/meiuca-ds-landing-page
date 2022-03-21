5c8470bd16216bd3039be152bcdf58fd26724d40
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-21 14:40:36 -0300

    feat: config breakpoint, spacing and apply grid on App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      md: '768px',
+      lg: '1366px',
+      xl: '1440px'
+    },
+    extend: {
+      spacing: {
+        '1': '8px',
+        '2': '16px',
+        '3': '24px'
+      }
+    },
   },
   plugins: [],
 }
