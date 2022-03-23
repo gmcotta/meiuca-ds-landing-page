9d796e9f9e6b1f8a0153143af6d3c02deefe8f20
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-23 18:42:13 -0300

    feat(VectorToRotate): create component

---
+++ src/components/VectorToRotate/index.jsx
@@ -0,0 +1,9 @@
+import { ReactComponent as Vector } from './Vector.svg';
+
+import './vector-to-rotate.styles.scss';
+
+export default function VectorToRotate() {
+  return (
+    <Vector className="vector" />
+  )
+}
