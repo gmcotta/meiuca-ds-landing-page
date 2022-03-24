49899358f976dd8ca17a445ac9e524f640211b26
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-24 09:48:13 -0300

    feat: add repo link

--- src/sections/Footer/index.jsx
+++ src/sections/Footer/index.jsx
@@ -8,7 +8,11 @@ export default function AppFooter() {
   return (
     <footer className="section-footer">
       <dsc-icon icon={iconGithub} size="lg"></dsc-icon>
-      <dsc-link href="#" target="_blank">
+      <dsc-link 
+        href="https://github.com/gmcotta/meiuca-ds-landing-page" 
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         Repositório do projeto
       </dsc-link>
     </footer>
